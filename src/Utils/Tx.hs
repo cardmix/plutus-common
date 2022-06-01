28df2a4542b0305da4d53a66cddd62ea72651c98
cardmix <91214090+cardmix@users.noreply.github.com>
2022-06-01 19:36:54 +0200

    Add textToCardanoTx

--- src/Utils/Tx.hs
+++ src/Utils/Tx.hs
@@ -12,12 +12,12 @@
 
 module Utils.Tx where
 
-import           Cardano.Api.Shelley     (serialiseToCBOR)
-import           Data.Aeson.Extras       (encodeByteString)
+import           Cardano.Api.Shelley     (EraInMode (..), AsType (..), SerialiseAsCBOR (..))
+import           Data.Aeson.Extras       (encodeByteString, tryDecode)
 import           Data.Default            (def)
 import           Data.Text               (Text)
 import           Ledger.Constraints      (UnbalancedTx)
-import           Ledger.Tx.CardanoAPI    (ToCardanoError)
+import           Ledger.Tx               (CardanoTx (..), SomeCardanoApiTx (..))
 import           Plutus.Contract.Wallet  (ExportTx (..), export)
 import           PlutusTx.Prelude        hiding ((<>))
 
@@ -25,6 +25,12 @@ import           Utils.Network
 
 ------------------------ Export/Import of transactions -------------------------
 
--- UnbalancedTx to CBOR conversion
-unbalancedTxToCBOR :: NetworkConfig -> UnbalancedTx -> Either ToCardanoError Text
-unbalancedTxToCBOR NetworkConfig {networkID, pparams} = fmap (encodeByteString . serialiseToCBOR . partialTx) . export pparams networkID def
+unbalancedTxToCBOR :: NetworkConfig -> UnbalancedTx -> Maybe Text
+unbalancedTxToCBOR NetworkConfig {networkID, pparams} = fmap (encodeByteString . serialiseToCBOR . partialTx) .
+    either (const Nothing) Just . export pparams networkID def
+
+textToCardanoTx :: Text -> Maybe CardanoTx
+textToCardanoTx txt = do
+    bs <- either (const Nothing) Just $ tryDecode txt
+    tx <- either (const Nothing) Just $ deserialiseFromCBOR AsAlonzoTx bs
+    return $ CardanoApiTx $ SomeTx tx AlonzoEraInCardanoMode
