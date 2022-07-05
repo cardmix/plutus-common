89de5e036ba9518866511687982371429f03bad0
cardmix <91214090+cardmix@users.noreply.github.com>
2022-07-05 12:17:10 +0200

    Chain index queries redesign

--- src/Contracts/ChainIndex.hs
+++ src/Contracts/ChainIndex.hs
@@ -8,7 +8,6 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE NumericUnderscores         #-}
-{-# LANGUAGE OverloadedLists            #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeFamilies               #-}
@@ -17,24 +16,20 @@
 module Contracts.ChainIndex where
 
 import           Cardano.Api                       (FromJSON, ToJSON)
-import           Data.Default                      (def)
-import           Data.Map                          (Map, fromList)
-import           Data.Maybe                        (catMaybes, fromJust)
+import           Control.Monad.Extra               (mconcatMapM)
+import           Data.Map                          (Map, union)
 import           GHC.Generics                      (Generic)
 import           Ledger                            (Address, ChainIndexTxOut(..), TxOutRef, POSIXTime, AssetClass)
-import           Ledger.Tx                         (TxOut(..), txOutRefId, toTxOut)
-import           Plutus.ChainIndex                 (ChainIndexTx, Page(..), nextPageQuery)
-import           Plutus.ChainIndex.Api             (UtxosResponse (page))
-import           Plutus.Contract                   (AsContractError, Contract, txOutFromRef, currentTime)
-import           Plutus.Contract.Request           (txsFromTxIds, unspentTxOutFromRef, utxoRefsAt, utxoRefsWithCurrency)
-import           PlutusTx.Prelude                  hiding ((<>))
-import           Prelude                           (Show, (<>))
+import           Plutus.ChainIndex                 (ChainIndexTx)
+import           PlutusTx.Prelude                  hiding ((<>), (<$>))
+import           Prelude                           (Show, undefined, IO, (<$>))
 
 
 data ChainIndexCache = ChainIndexCache {
-    cacheAddress :: Address,
-    cacheData    :: Map TxOutRef (ChainIndexTxOut, ChainIndexTx),
-    cacheTime    :: POSIXTime
+    cacheAddresses  :: [Address],
+    cacheCurrencies :: [AssetClass],
+    cacheData       :: Map TxOutRef (ChainIndexTxOut, ChainIndexTx),
+    cacheTime       :: POSIXTime
 }
     deriving (Show, Generic, FromJSON, ToJSON)
 
@@ -42,75 +37,26 @@ data ChainIndexCache = ChainIndexCache {
 cacheValidityPeriod :: POSIXTime
 cacheValidityPeriod = 30_000
 
-updateChainIndexCache :: AsContractError e => ChainIndexCache -> Contract w s e ChainIndexCache
-updateChainIndexCache oldCache@(ChainIndexCache cAddr _ cTime) = do
+updateChainIndexCache :: ChainIndexCache -> IO ChainIndexCache
+updateChainIndexCache oldCache@(ChainIndexCache addrs curs _ cTime) = do
     curTime <- currentTime
     if curTime - cTime <= cacheValidityPeriod
         then return oldCache
         else do
-            utxos <- getUtxosAt cAddr
-            ChainIndexCache cAddr utxos <$> currentTime
+            utxos'  <- mconcatMapM getUtxosAt addrs
+            utxos'' <- mconcatMapM getUtxosWithCurrency curs
+            let utxos = utxos' `union` utxos''
+            ChainIndexCache addrs curs utxos <$> currentTime
 
-txOutsFromRefs :: forall w s e. (AsContractError e) => [TxOutRef] -> Contract w s e [TxOut]
-txOutsFromRefs refs = map toTxOut <$> (catMaybes <$> traverse txOutFromRef refs)
+currentTime :: IO POSIXTime
+currentTime = undefined
 
------------------------------------ Custom queries -------------------------------
+----------------------------------- Chain index queries ---------------------------------
 
--- Does the same as utxosTxOutTxAt
-getUtxosAt :: forall w s e. (AsContractError e) => Address -> Contract w s e (Map TxOutRef (ChainIndexTxOut, ChainIndexTx))
-getUtxosAt addr = do
-    utxoRefs <- foldUtxoRefsAt f [] addr
-    txOuts <- traverse unspentTxOutFromRef utxoRefs
-    let txIds = txOutRefId <$> utxoRefs 
-    txs <- txsFromTxIds txIds
-    return $ fromList $ map (\(ref, (txo, tx)) -> (ref, (fromJust txo, tx))) $ filter (isJust . fst . snd) $ zip utxoRefs $ zip txOuts txs
-  where
-      f acc pg = pure $ acc <> pageItems pg
+-- Get all utxos at a given address
+getUtxosAt :: Address -> IO (Map TxOutRef (ChainIndexTxOut, ChainIndexTx))
+getUtxosAt _ = undefined
 
 -- Get all utxos containing a given currency
-getUtxosWithCurrency :: forall w s e. (AsContractError e) => AssetClass -> Contract w s e (Map TxOutRef (ChainIndexTxOut, ChainIndexTx))
-getUtxosWithCurrency addr = do
-    utxoRefs <- foldUtxoRefsWithCurrency f [] addr
-    txOuts <- traverse unspentTxOutFromRef utxoRefs
-    let txIds = txOutRefId <$> utxoRefs 
-    txs <- txsFromTxIds txIds
-    return $ fromList $ map (\(ref, (txo, tx)) -> (ref, (fromJust txo, tx))) $ filter (isJust . fst . snd) $ zip utxoRefs $ zip txOuts txs
-  where
-      f acc pg = pure $ acc <> pageItems pg
-
--- | Fold through each 'Page's of unspent 'TxOutRef's at a given 'Address', and
--- accumulate the result.
-foldUtxoRefsAt ::
-    forall w s e a.
-    ( AsContractError e
-    )
-    => (a -> Page TxOutRef -> Contract w s e a) -- ^ Accumulator function
-    -> a -- ^ Initial value
-    -> Address -- ^ Address which contain the UTXOs
-    -> Contract w s e a
-foldUtxoRefsAt f ini addr = go ini (Just def)
-  where
-    go acc Nothing = pure acc
-    go acc (Just pq) = do
-      pg <- page <$> utxoRefsAt pq addr
-      newAcc <- f acc pg
-      go newAcc (nextPageQuery pg)
-
-
--- | Fold through each 'Page's of unspent 'TxOutRef's with a given Currency, and
--- accumulate the result.
-foldUtxoRefsWithCurrency ::
-    forall w s e a.
-    ( AsContractError e
-    )
-    => (a -> Page TxOutRef -> Contract w s e a) -- ^ Accumulator function
-    -> a -- ^ Initial value
-    -> AssetClass -- ^ Address which contain the UTXOs
-    -> Contract w s e a
-foldUtxoRefsWithCurrency f ini ac = go ini (Just def)
-  where
-    go acc Nothing = pure acc
-    go acc (Just pq) = do
-      pg <- page <$> utxoRefsWithCurrency pq ac
-      newAcc <- f acc pg
-      go newAcc (nextPageQuery pg)
+getUtxosWithCurrency :: AssetClass -> IO (Map TxOutRef (ChainIndexTxOut, ChainIndexTx))
+getUtxosWithCurrency _ = undefined
