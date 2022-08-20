d6976700acf0403d78cd735c633cf2fa8d1a5768
Cauterization <71918928+Cauterization@users.noreply.github.com>
2022-08-20 18:56:33 +0300

    Fix bech32ToAddress
    Added contract addresses

--- src/Utils/Address.hs
+++ src/Utils/Address.hs
@@ -12,17 +12,17 @@ module Utils.Address where
 
 import           Data.Text                       (Text)
 import           Data.Foldable                   (asum)
-import           Cardano.Api.Shelley             (AsType (..), StakeAddress(..), StakeAddressReference (..), StakeCredential (..), Hash (..),
-                                                  deserialiseFromBech32, fromShelleyStakeReference)
-import qualified Cardano.Api.Shelley             as Shelley
+import           Cardano.Api.Shelley             (AsType (..), StakeAddress(..),
+                                                  deserialiseFromBech32)
 import           Cardano.Chain.Common            (decodeAddressBase58)
 import           Cardano.Ledger.Alonzo.TxInfo    (transKeyHash)
 import           Cardano.Ledger.Address          (bootstrapKeyHash, BootstrapAddress (BootstrapAddress))
-import           Cardano.Ledger.Credential       (Credential(..))
 import qualified Cardano.Ledger.Credential       as Shelley
 import           Cardano.Ledger.Crypto           (StandardCrypto)
 import           Control.FromSum                 (eitherToMaybe)
-import           Ledger.Address                  (PaymentPubKeyHash(..), StakePubKeyHash(..), Address(..), pubKeyHashAddress, toPubKeyHash, stakingCredential)
+import           Ledger.Address                  (PaymentPubKeyHash(..), StakePubKeyHash(..), Address(..), pubKeyHashAddress, toPubKeyHash, 
+                                                  stakingCredential)
+import           Ledger.Tx.CardanoAPI            (fromCardanoAddress)
 import           Plutus.V1.Ledger.Credential     (Credential(..), StakingCredential(..))
 import           PlutusTx.Prelude                hiding (asum, error)
 
@@ -34,7 +34,7 @@ bech32ToKeyHashes txt = do
     let skh = case stakingCredential addr of
             Just (StakingHash (PubKeyCredential spkh)) -> Just $ StakePubKeyHash spkh
             Just (StakingHash (ScriptCredential vh))   -> Nothing
-            Just (StakingPtr{})                        -> Nothing -- yet
+            Just (StakingPtr{})                        -> Nothing -- yet?
             Nothing                                    -> Nothing
     pure (PaymentPubKeyHash pkh, skh)
 
@@ -44,19 +44,13 @@ bech32ToAddress txt = asum $ map ($ txt) [bech32ToShelley, bech32ToByron]
 
 bech32ToShelley :: Text -> Maybe Address
 bech32ToShelley txt = do
-    Shelley.ShelleyAddress _ payCred stakeRef  <- eitherToMaybe $ deserialiseFromBech32 AsShelleyAddress txt
-    pkh <- case payCred of
-            KeyHashObj    h1 -> Just $ PaymentPubKeyHash $ transKeyHash h1
-            ScriptHashObj _  -> Nothing
-    let skh = case fromShelleyStakeReference stakeRef of
-            StakeAddressByValue (StakeCredentialByKey (StakeKeyHash h2)) -> Just $ StakePubKeyHash $ transKeyHash h2
-            _  -> Nothing
-    pure $ pubKeyHashAddress pkh skh
+    sAddr <- eitherToMaybe (deserialiseFromBech32 AsShelleyAddress txt)
+    eitherToMaybe $ fromCardanoAddress sAddr
 
 bech32ToByron :: Text -> Maybe Address
-bech32ToByron txt =  do 
-    bAddr <- eitherToMaybe $ decodeAddressBase58 txt
-    pure $ flip pubKeyHashAddress Nothing $ PaymentPubKeyHash $ transKeyHash $ bootstrapKeyHash @StandardCrypto $ BootstrapAddress bAddr  
+bech32ToByron txt =  fromByron <$> eitherToMaybe (decodeAddressBase58 txt)
+    where
+        fromByron = flip pubKeyHashAddress Nothing . PaymentPubKeyHash . transKeyHash . bootstrapKeyHash @StandardCrypto . BootstrapAddress
 
 -- Convert bech32 Stake address to a Plutus StakePubKeyHash.
 bech32ToStakePubKeyHash :: Text -> Maybe StakePubKeyHash
