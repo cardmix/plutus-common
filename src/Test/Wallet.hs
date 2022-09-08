4a8355f06e31feea56c7d0baf30734652407cef7
cardmix <111982001+encryptedcoins@users.noreply.github.com>
2022-09-08 22:01:14 +0200

    Fix tests

--- src/Test/Wallet.hs
+++ src/Test/Wallet.hs
@@ -11,7 +11,7 @@
 {-# LANGUAGE TypeApplications           #-}
 
 module Test.Wallet where
-    
+
 import           Cardano.Api.Shelley                 (NetworkId(..), ProtocolParameters (..), NetworkMagic(..))
 import           Cardano.Mnemonic                    (MkSomeMnemonic(..))
 import           Cardano.Wallet.Primitive.Passphrase (Passphrase(..))
@@ -32,20 +32,20 @@ import           Utils.Address                       (bech32ToKeyHashes)
 daedalusAddress :: Text
 daedalusAddress = "addr_test1qpmv0wkr6z9sdqveecpuywrwcxyueft0wgle85cs9fhsvtgnt9a4spnfrrlpp7puw2lcx2zudf49ewyza4q9ha08qhdq7aezrw"
 
-test :: IO [TxOutRef]
-test = do
-  pp <- fromJust . decode <$> readFile "testnet/protocol-parameters.json" :: IO ProtocolParameters
+test :: FilePath -> IO [TxOutRef]
+test file = do
+  pp <- fromJust . decode <$> readFile file :: IO ProtocolParameters
   let (pkh, skh) = fromJust $ bech32ToKeyHashes daedalusAddress
       networkId = Testnet $ NetworkMagic 1097911063
       ledgerParams = Params def pp networkId
   getWalletTxOutRefs ledgerParams pkh skh 1
 
 testWalletGen :: Bool
-testWalletGen = "2233e2fc50a0d880e187f4a87de74234e960bd95" == show 
-    (getWalletId $ genWalletId 
-    (fromRight undefined $ mkSomeMnemonic @'[ 24 ] . T.words $ 
+testWalletGen = "2233e2fc50a0d880e187f4a87de74234e960bd95" == show
+    (getWalletId $ genWalletId
+    (fromRight undefined $ mkSomeMnemonic @'[ 24 ] . T.words $
     "chronic distance live brand switch angry erase empty lobster defy disorder flush moon burst acoustic miracle creek wild excuse spike fork neutral market shuffle")
     (Passphrase $ fromString "1234567890"))
 
-testWaletIdFromFile :: IO WalletId
-testWaletIdFromFile = walletIdFromFile "src/Test/restore-wallet.json"
+testWaletIdFromFile :: FilePath -> IO WalletId
+testWaletIdFromFile = walletIdFromFile
