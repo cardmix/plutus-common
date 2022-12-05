b675e1d1cc9dcad7f91fdc9dafbd75c1c9bfe7e3
encoins <111982001+encryptedcoins@users.noreply.github.com>
2022-12-05 15:55:31 +0100

    Add Arbitrary PubKeyHash instance

--- src/Utils/Orphans.hs
+++ src/Utils/Orphans.hs
@@ -13,6 +13,8 @@
 
 module Utils.Orphans where
 
+import           Ledger                            (PubKeyHash(..), StakePubKeyHash)
+import           Ledger.Address                    (PaymentPubKeyHash (..), StakePubKeyHash (..))
 import           Ledger.Tx                         (TxOutRef (..), TxId (..))
 import           PlutusTx.Prelude                  hiding ((<$>), (<>))
 import           Prelude                           ((<$>), (^))
@@ -24,3 +26,14 @@ instance Arbitrary TxOutRef where
     arbitrary = do
         bs <- arbitrary
         TxOutRef (TxId $ toBytes $ modulo bs (2 ^ (256 :: Integer) - 1)) <$> arbitrary
+
+instance Arbitrary PubKeyHash where
+    arbitrary = do
+        bs <- arbitrary
+        return $ PubKeyHash $ toBytes $ modulo bs (2 ^ (256 :: Integer) - 1)
+
+instance Arbitrary PaymentPubKeyHash where
+    arbitrary = PaymentPubKeyHash <$> arbitrary
+
+instance Arbitrary StakePubKeyHash where
+    arbitrary = StakePubKeyHash <$> arbitrary
