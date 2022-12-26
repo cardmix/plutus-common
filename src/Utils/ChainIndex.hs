301f86cb91dcdf8c8fd284be339f44c2a9aed3ee
encoins <111982001+encryptedcoins@users.noreply.github.com>
2022-12-26 22:45:12 +0100

    Fix filterCleanUtxos

--- src/Utils/ChainIndex.hs
+++ src/Utils/ChainIndex.hs
@@ -3,7 +3,6 @@ module Utils.ChainIndex where
 import qualified Data.Map as Map
 import           Ledger            (TxOutRef(..), DecoratedTxOut(..))
 import           Ledger.Ada        (fromValue, toValue)
-import           Plutus.ChainIndex (ChainIndexTx)
 
-filterCleanUtxos :: Map.Map TxOutRef (DecoratedTxOut, ChainIndexTx) -> Map.Map TxOutRef (DecoratedTxOut, ChainIndexTx)
-filterCleanUtxos = Map.filter $ (\v -> toValue (fromValue v) == v) . _decoratedTxOutValue . fst
+filterCleanUtxos :: Map.Map TxOutRef DecoratedTxOut -> Map.Map TxOutRef DecoratedTxOut
+filterCleanUtxos = Map.filter $ (\v -> toValue (fromValue v) == v) . _decoratedTxOutValue
